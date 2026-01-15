96262e08a63783c6b52843ab92e561f7a4464c39
NicolleValovicova <302245247@student.rocmondriaan.nl>
2026-01-15 14:31:13 +0100

    add jump to top btn, finish the header jumping to pages on click.

--- js/main.js
+++ js/main.js
@@ -1,28 +1,50 @@
 //  script for scrollbar cards
-const swiper = new Swiper('.swiper', {
-    loop: true,
-    spaceBetween: 30,
+const swiper = new Swiper(".projects-container", {
+  loop: true,
+  spaceBetween: 30,
   // If we need pagination
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
     clickable: true,
-    dynamicBullets: true
+    dynamicBullets: true,
   },
 
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
   breakpoints: {
-    0:{
-        slidesPerView: 1
+    0: {
+      slidesPerView: 1,
     },
-    768:{
-        slidesPerView: 2
+    768: {
+      slidesPerView: 2,
     },
-    1024:{
-        slidesPerView: 3
+    1024: {
+      slidesPerView: 3,
     },
-  }
+  },
 });
+
+// flag select stuff change language
+let dutchFlag = document.querySelector("#nederlands");
+let englishFlag = document.querySelector("#engels");
+
+let currentlyActive = 1;
+
+function toggle() {
+  currentlyActive *= -1;
+  
+  if (currentlyActive < 0) {
+    dutchFlag.classList.add("hidden");
+    englishFlag.classList.remove("hidden");
+  } else {
+    dutchFlag.classList.remove("hidden");
+    englishFlag.classList.add("hidden");
+  }
+}
+
+
+dutchFlag.addEventListener("click", toggle);
+englishFlag.addEventListener("click", toggle);
